a3dd5096908fb4364118c17e2c31c1c61183d7b4
kaitg <kaitgaston8@gmail.com>
2026-08-08 15:24:52 -0400

    Shader lesson done

--- 27-shaders/src/script.js
+++ 27-shaders/src/script.js
@@ -39,11 +39,21 @@ geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
 // Material
 const material = new THREE.RawShaderMaterial({
     vertexShader:testVertexShader,
-    fragmentShader:testFragmentShader
+    fragmentShader:testFragmentShader,
+    uniforms:
+    {
+        uFrequency: { value: new THREE.Vector2(10, 5) },
+        uTime: { value: 0 },
+        uColor: { value: new THREE.Color('orange') }
+    }
 })
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name("frequencyX")
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name("frequencyY")
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
+mesh.scale.y = 2/3
 scene.add(mesh)
 
 /**
@@ -98,6 +108,8 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
--- 27-shaders/src/shaders/test/fragment.glsl
+++ 27-shaders/src/shaders/test/fragment.glsl
@@ -1,8 +1,9 @@
 precision mediump float;
+uniform vec3 uColor;
 
 varying float vRandom;
 
 void main()
 {
-    gl_FragColor = vec4(vRandom, vRandom*0.5, 1.0, 1.0);
+    gl_FragColor = vec4(vRandom, vRandom * 0.5, 1.0, 1.0);
 }
